d9d43fa12b3e8ae43cc4046efe70f1497e6c8dbf
Matthew Mantle <memantle@googlemail.com>
2017-01-17 11:23:30 +0000

    Tidied up instructions

--- index.php
+++ index.php
@@ -11,12 +11,10 @@ a) The following code shows the class being used to create a Student object. Unc
 b) Add some additional code below to create a second student object. 
 */
 
-
-use OOPPractical\Student;
-use OOPPractical\StudentPrinter;
-require_once("student.php");
-require_once("student-printer.php");
 /*
+use OOPPractical\Student;
+
+require_once("student.php");
 $exampleStudent = new Student();
 var_dump($exampleStudent);
 $exampleStudent->studentNum="u0123456";
@@ -27,44 +25,32 @@ echo "<p>".$exampleStudent->getFullName()."</p>";
 */
 
 /*
-2) Modify your Student class to use a constructor function. The code below provides an example of the constructor function being called. Uncomment this to check it works (the code above will now give you errors because it doesn't use the constructor - comment it out again). Again, once this works add some additional code to create a seconds student object
+2) Modify your Student class to use a constructor function. The code below provides an example of the constructor function being called. Uncomment this to check it works (the code from Q1 above will now give you errors because it doesn't use the constructor - comment it out again). Again, once this works add some additional code to create a seconds student object
 */
 
+/*
 $exampleStudent = new Student("u0123456", "John", "Smith");
 echo "<p>".$exampleStudent->getFullName()."</p>";
+*/
 
 
 /*
 3) The following code creates several instances of Student and stores them in an array. Uncomment the code and add a foreach loop that will output each student's name in turn. 
 */
 
-
+/*
 $students=[];
 $students[]= new Student("u0123456", "John", "Smith");
 $students[]= new Student("u0123456", "Ruhksar", "Mirza");
 $students[]= new Student("u0123456", "Ania", "Kowalski");
-
-
-
-foreach($students as $student){
-	echo "<p>".$student->getFullName()."</p>";
-}
+*/
 
 
 /*
 4) Open up the file StudentPrinter. It has a single method print(). 
-a) Write some code below file that will create an instance of StudentPrinter, and call the printStudents() method so that the array of students is displayed.
+a) Write some code in this file (index.php) that will create an instance of StudentPrinter, and call the printStudents() method so that the names of all students are displayed.
+b) Add an additional method to the StudentPrinter class, name it printStudentsAsList(). This method should output the array of students as an HTML list. Check this works by calling the printStudentsAsList() method.
 */
-
-
-$studentPrinter = new StudentPrinter();
-$studentPrinter->printStudents($students);
-$studentPrinter->printStudentsAsList($students);
-
-/*
-b) Add an additional method to this class, name it printStudentsAsList(). This methods should output the array of students as an HTML list. Check this works by calling the printStudentsAsList() method.
-*/
-
 
 /*
 5) Have a look at the slides for info about access modifiers. Make the properties in the Student class private. 
