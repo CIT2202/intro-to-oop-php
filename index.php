bf43590a1791a8d69889e611b2b9a19604889608
mmantle-hud <m.e.mantle@hud.ac.uk>
2018-02-26 10:46:25 +0000

    Tidied up the questions

--- index.php
+++ index.php
@@ -12,53 +12,13 @@ class StudentPrinter{
            echo "<p>".$student->getFullName()."</p>";
         }
     }
-    static function printStudentsAsList($students)
-    {
-        echo "<ul>";
-        foreach($students as $student){
-           echo "<li>".$student->getFullName()."</li>";
-        }
-        echo "</ul>";
-    }
 }
 
 class Student{
-	private $studentNum;
-	private $firstName;
-	private $lastName;
-	function __construct($studentNum,$firstName,$lastName)
-	{
-		$this->setStudentNum($studentNum);
-	 	$this->setFirstName($firstName);
-	 	$this->setLastName($lastName);
-	}
-	public function setStudentNum($studentNum)
-	{
-		if($studentNum==="")  {
-            throw  new  InvalidArgumentException ( "Student number can't be empty" ) ;
-        }
-        if($studentNum[0]!=="u")  {
-            throw  new  InvalidArgumentException ( "Student number must start with a 'u'" ) ;
-        } 
-        if(strlen($studentNum)!==8)  {
-            throw  new  InvalidArgumentException ( "Student number must be 8 characters in length" ) ;
-        } 
-		$this->studentNum=$studentNum;
-	}
-	public function setLastName($lastName)
-	{
-		if($lastName==="")  {
-            throw  new  InvalidArgumentException ( "Last name can't be empty" ) ;
-        } 
-		$this->lastName=$lastName;
-	}
-	public function setFirstName($firstName)
-	{
-		if($firstName==="")  {
-            throw  new  InvalidArgumentException ( "First name can't be empty" ) ;
-        } 
-		$this->firstName=$firstName;
-	}
+	public $studentNum;
+	public $firstName;
+	public $lastName;
+	
 	public function getFullName()
 	{
 		return $this->firstName." ".$this->lastName;
@@ -100,12 +60,9 @@ c) Add some additional to create a second student object. Use var_dump() to chec
 // $students[]= new Student("u0123456", "Ruhksar", "Mirza");
 // $students[]= new Student("u0123456", "Ania", "Kowalski");
 
-// StudentPrinter::printStudents($students);
-// StudentPrinter::printStudentsAsList($students);
-
 /*
-4) Open up the file StudentPrinter. It has a single method print(). 
-a) Write some code in this file (index.php) that will create an instance of StudentPrinter, and call the printStudents() method so that the names of all students are displayed.
+4) Uncomment the class StudentPrinter. It has a single method printStudents(). 
+a) Write some code that will call the printStudents() method so that the names of all students are displayed (note printStudents is a static method). Once this works you can delete the foreach loop you added in (Q3).
 b) Add an additional method to the StudentPrinter class, name it printStudentsAsList(). This method should output the array of students as an HTML list. Check this works by calling the printStudentsAsList() method.
 */
 
@@ -118,11 +75,11 @@ b) Try adding additional checks for the student number e.g. it must start with a
 
 //testing getters and setters
 
-// $student = new Student("u0123456", "John", "Smith");
-// $student = new Student("0123456", "John", "Smith");
-// $student = new Student("u012345", "John", "Smith");
-// $student = new Student("u0123456", "", "Smith");
-// $student = new Student("u0123456", "John", "");
+// $student = new Student("u0123456", "John", "Smith"); //should work ok
+// $student = new Student("0123456", "John", "Smith"); //should give an error (no u in the student number)
+// $student = new Student("u012345", "John", "Smith"); //should give an error (student number not longe enough)
+// $student = new Student("u0123456", "", "Smith"); //should given an error (empty first name)
+// $student = new Student("u0123456", "John", ""); //should given an error (empty kast name)
 
 ?>
 </body>
