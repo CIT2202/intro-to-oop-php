1a4bf201c42de0b9513b4197ae4eb7695df81530
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-11-25 16:10:34 +0000

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ c) Add some additional to create a second student object. Use var_dump() to chec
 
 
 // $exampleStudent = new Student("u0123456", "John", "Smith");
-// echo "<p>".$exampleStudent->getFullName()."</p>";
+// echo "<p>{$exampleStudent->getFullName()}</p>";
 
 
 
@@ -99,19 +99,26 @@ c) Add some additional to create a second student object. Use var_dump() to chec
 // $students[]= new Student("u0123456", "John", "Smith");
 // $students[]= new Student("u0123456", "Ruhksar", "Mirza");
 // $students[]= new Student("u0123456", "Ania", "Kowalski");
-
+	
+// foreach($students as $student){
+//   echo "<p>{$student->getFullName()}</p>";
+// }
+	
 /*
 4) Uncomment the class StudentPrinter. It has a single method printStudents(). 
 a) Write some code that will call the printStudents() method so that the names of all students are displayed (note printStudents is a static method). Once this works you can delete the foreach loop you added in (Q3).
 b) Add an additional method to the StudentPrinter class, name it printStudentsAsList(). This method should output the array of students as an HTML list. Check this works by calling the printStudentsAsList() method.
 */
-
+	
+//StudentPrinter::printStudents($students);
+	
 /*
 5) Have a look at the slides for info about access modifiers. Make the properties in the Student class private. 
 a) Add setter methods so that values for these properties can be set. If you can get this to work, add some checks to the setter methods to make sure suitable values have been used. To start with, keep it simple, just check for empty strings.  
 b) Try adding additional checks for the student number e.g. it must start with a 'u' and be exactly eight characters in length. 
 */
-
+	
+//StudentPrinter::printStudentsAsList($students);
 
 //testing getters and setters
 
